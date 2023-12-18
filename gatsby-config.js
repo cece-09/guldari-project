5a8b361b2e45b168f642ca9c209d2a40e00f0123
cece <sojeong.lee017@gmail.com>
2023-12-18 23:31:30 +0900

    chore: add netlify plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,39 +1,39 @@
-require('dotenv').config({
-  path: `.env.${process.env.NODE_ENV}`,
-})
+require("dotenv").config({
+    path: `.env.${process.env.NODE_ENV}`,
+});
 
 module.exports = {
-  siteMetadata: {
-    title: `리마켓프로젝트`,
-    description: `리마켓프로젝트 웹사이트입니다.`,
-    image: '/site-image.png',
-    author: `@gc_guldari`,
-    siteUrl: `https://gcguldari.gatsbyjs.io/`,
-  },
-  plugins: [
-    `gatsby-plugin-image`,
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-source-contentful`,
-      options: {
-        spaceId: process.env.CONTENTFUL_SPACE_ID,
-        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-      },
+    siteMetadata: {
+        title: `리마켓프로젝트`,
+        description: `리마켓프로젝트 웹사이트입니다.`,
+        image: "/site-image.png",
+        author: `@gc_guldari`,
+        siteUrl: `https://gcguldari.gatsbyjs.io/`,
     },
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        plugins: [],
-      },
-
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/data`,
-      },
-    },
-  ],
-}
+    plugins: [
+        `gatsby-plugin-image`,
+        `gatsby-transformer-sharp`,
+        `gatsby-plugin-sharp`,
+        `gatsby-plugin-netlify`,
+        {
+            resolve: `gatsby-source-contentful`,
+            options: {
+                spaceId: process.env.CONTENTFUL_SPACE_ID,
+                accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+            },
+        },
+        {
+            resolve: `gatsby-transformer-remark`,
+            options: {
+                plugins: [],
+            },
+        },
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `images`,
+                path: `${__dirname}/src/data`,
+            },
+        },
+    ],
+};
